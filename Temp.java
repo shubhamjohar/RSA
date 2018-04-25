9cc8807d48ec5a8aa6deba021e5faee9c94d38b1
shubhamj <shubhamjohar024@gmail.com>
2018-04-25 23:37:43 +0530

    Added comments

--- Temp.java
+++ Temp.java
@@ -6,8 +6,22 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 
-public class Temp {
+/*
+------------__RSA__------------------
 
+This RSA implementation uses block size as 6, that is it converts the text message into blocks with
+6 characters and pads zeroes when necessary.
+
+e is set as 65537
+
+For number of bits in the JFrame add integers values only such as 1024,3072.
+The code has been successfully tested on 4096 bits.
+
+Enter message and click on button to see RSA work
+
+*/
+
+public class Temp {
 	// function to return prime number p annd q 	
     static BigInteger big_primes(int bitLength){
         Random rnd = new Random();
@@ -24,22 +38,19 @@ public class Temp {
 	   return a.gcd(b);
     }    
 
-    static String ascii_string(String input_string_block){
-        String temp = "";
-        for(int i=0;i<input_string_block.length();i++){
-            temp += (int)input_string_block.charAt(i);
-        }
-        return temp;
-    }
-	
+    // The  encrypt() of the RSA algorithm, cipher =(message^e)%n
     static BigInteger encrypt(BigInteger message_i,BigInteger e, BigInteger n){
 	   //System.out.println(" Message is : "+message_i);
        return message_i.modPow(e,n);
     }
 
+    // The  decrypt of the RSA algorithm, message =(cipher^d)%n
     static BigInteger decrypt(BigInteger cipher_i, BigInteger d, BigInteger n){
 	   return cipher_i.modPow(d,n);
     }
+
+    // a simple function that returns a given string 'n' time
+    // Ex --  repear_string_n_times("0",3) gives "000"
     static String repeat_string_n_times(String str,int n){
         String temp="";
         if(n>0){
@@ -62,27 +73,24 @@ public class Temp {
         return temp;
     }
 
-
   public static void main(String[] args) {
+
+       // ----------------GUI code starts----------------
        //Creating the Frame
         JFrame frame = new JFrame("RSA ");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(700,900);
 
-        //Creating the panel at bottom and adding components
-        JPanel panel = new JPanel(); // the panel is not visible in output
+        //Creating the panel for  adding components
+        JPanel panel = new JPanel();
 
-
+        // adding title to the JPanel
         TitledBorder border = new TitledBorder("RSA Algorithm");
         border.setTitleJustification(TitledBorder.CENTER);
         border.setTitlePosition(TitledBorder.TOP);
         panel.setBorder(border);
 
         JLabel no_of_bits = new JLabel("Enter number of bits for the  RSA public key ");
-        JLabel p = new JLabel();
-        JLabel q = new JLabel();
-        JLabel phi = new JLabel();
-        JLabel e_RSA = new JLabel();
         JTextField no_bits = new JTextField(10); // accepts upto 10 characters
         JButton send = new JButton("Start RSA execution");
         JLabel message_label = new JLabel("Enter the message for RSA algorithm");
@@ -101,10 +109,12 @@ public class Temp {
         JScrollPane scroll_q = new JScrollPane (message_textArea);
         scroll_q.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scroll_p.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-
+        
+        // when the 'Start RSA execution ' button is clicked
         send.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){  
                 int bitLength = Integer.parseInt(no_bits.getText());
+                p_q_phi_textArea.append(" Starting RSA with public key bit length as "+bitLength+" and block size as 6.\n\n");
                 BigInteger p,q,n,phi,e_R;
                 p = big_primes(bitLength/2);
                 p_q_phi_textArea.append(" PUBLIC KEY 'p' is : "+p+"\n\n");
@@ -123,34 +133,44 @@ public class Temp {
                     number_of_zeros_appended =6- (message.length() % 6);
                     message += repeat_string_n_times("0",number_of_zeros_appended);
                 }
+
+                p_q_phi_textArea.append(" message after padding "+number_of_zeros_appended+" zeroes is "+message+"\n\n");
                 p_q_phi_textArea.append(" Number of blocks is : "+ message.length()/6+"\n\n");
+
+                // BigIntger[] for storing encrypted and decrypted message blocks
+                // block size is 6
                 BigInteger[] cipher_i = new BigInteger[message.length()/6];
                 BigInteger[] message_i = new BigInteger[message.length()/6];
                 String[] mess = new String[message.length()/6];
                 String[] temp_ascii_string = new String[message.length()/6];
-                String[] message_array = message.split("(?<=\\G.{6})");
+                String[] message_array = message.split("(?<=\\G.{6})");         // for splitting string into equal blocks of size 6
+
+                
+
                 for( int i=0; i<message_array.length; i++){
                     temp_ascii_string[i] = string_to_ascii(message_array[i]);
-                    p_q_phi_textArea.append(string_to_ascii(temp_ascii_string[i]));
+                    p_q_phi_textArea.append(" ASCII equivalent of the block["+Integer.toString(i+1)+"] is : "+temp_ascii_string[i]+"\n\n");
                     //cipher_i = encrypt(new BigInteger(string_to_ascii(temp_ascii_string)),e,n);
                     cipher_i[i] = encrypt(new BigInteger(message_array[i].getBytes()),e_R,n);
-                    p_q_phi_textArea.append("\n\n Encrypted text of block["+Integer.toString(i)+"] is : "+cipher_i[i]+"\n\n" );
+                    p_q_phi_textArea.append(" Encrypted text of block["+Integer.toString(i+1)+"] is : "+cipher_i[i]+"\n\n" );
                     message_i[i] = decrypt( cipher_i[i], d, n);
-                    p_q_phi_textArea.append("\n\n Decrypted text is : "+message_i[i] +"\n\n");
+                    //p_q_phi_textArea.append("\n\n Decrypted text is : "+message_i[i] +"\n\n");
                    // message_textArea.append("\n\n Original text is : "+ascii_to_string(new decrypt( cipher_i,d,n)));g
-                   mess[i] = new String(message_i[i].toByteArray());
-                    p_q_phi_textArea.append("\n\n Original text is : "+mess[i]+"\n\n");
-                }
+                    mess[i] = new String(message_i[i].toByteArray());
+                    p_q_phi_textArea.append(" Decrypted block["+Integer.toString(i+1)+"] text is : "+mess[i]+"\n\n");
+                } 
+
+                p_q_phi_textArea.append(" Decrypted Text message in ASCII with padding is "+String.join("",temp_ascii_string)+"\n\n");
+                
+                // showing the initial message( obtained by joining all mess[] elements)
                 String tttt = String.join("",mess);
-                p_q_phi_textArea.append(" Text message in ASCII with padding is "+String.join("",temp_ascii_string)+"\n\n");
-                p_q_phi_textArea.append("Original Text message is "+tttt.substring(0,message.length()-number_of_zeros_appended)+"\n\n");
+                p_q_phi_textArea.append("Original Text message after removing the padded zeroes is "+tttt.substring(0,message.length()-number_of_zeros_appended)+"\n\n");
 
                 String text = new String();
                 if(no_bits.getText().length() < 1){
                     JOptionPane.showMessageDialog(null, "Only numbers are allowed!");
                 }
                 else{
-                    
                     text = no_bits.getText();
                 }
                 
@@ -166,13 +186,9 @@ public class Temp {
         panel.add(message_textArea);  
         panel.add(send); 
         panel.add(scroll_p);  
-        panel.add(p); // Components Added using Flow Layout
-        panel.add(q);
-        panel.add(phi);
-        panel.add(e_RSA);
         
         //Adding Components to the frame.
-        frame.getContentPane().add( panel);
+        frame.getContentPane().add(panel);
         // frame.getContentPane().add(BorderLayout.CENTER, ta);
         frame.setVisible(true);
 
